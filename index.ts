c33278dab78d4607e045a8990c7e391dba6f6358
Andrew <andrewlmanley@gmail.com>
2020-02-01 16:33:34 -0500

    Added Storage methods

--- index.ts
+++ index.ts
@@ -1,3 +1,28 @@
-// import { browser } from 'webextension-polyfill-ts';
-export const testString = 'test string!';
-// export const browserExport = browser;
+import { browser } from 'webextension-polyfill-ts';
+
+export interface StorageResponse {
+  success: boolean;
+  data: string;
+}
+
+export const LoadFromStorage = async (key: string): Promise<StorageResponse> => {
+  let data: string;
+  try {
+    const response = await browser.storage.sync.get(key);
+    data = response[key];
+  } catch (error) {
+    data = null;
+  }
+  return {
+    success: data != null,
+    data: data,
+  };
+};
+
+export const SaveToStorage = async (key: string, data: string): Promise<void> => {
+  await browser.storage.sync.set({ [key]: data });
+};
+
+export const RemoveFromStorage = async (key: string): Promise<void> => {
+  await browser.storage.sync.remove(key);
+};
